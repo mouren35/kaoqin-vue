ec9a0e2b54c1973a97e66db73b7554c64c2f0ca9
d <new666@qq.com>
2022-09-03 10:52:31 +0800

    Vue3+TS4项目 - 初始化工程与整体架构

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store, { key } from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app.use(ElementPlus, { locale: zhCn }).use(store, key).use(router).mount('#app')
+
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,84 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+const Login = () => import('@/views/Login/Login.vue');
+const Home = () => import('@/views/Home/Home.vue');
+const Sign = () => import('@/views/Sign/Sign.vue');
+const Exception = () => import('@/views/Exception/Exception.vue');
+const Apply = () => import('@/views/Apply/Apply.vue');
+const Check = () => import('@/views/Check/Check.vue');
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    menu?: boolean
+    title?: string
+    icon?: string 
+    auth?: boolean
+  }
+}
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home,
+    redirect: '/sign',
+    meta: {
+      menu: true,
+      title: '考勤管理',
+      icon: 'document-copy',
+      auth: true
+    },
+    children: [
+      {
+        path: 'sign',
+        name: 'sign',
+        component: Sign,
+        meta: {
+          menu: true,
+          title: '在线打卡签到',
+          icon: 'calendar',
+          auth: true
+        },
+      },
+      {
+        path: 'exception',
+        name: 'exception',
+        component: Exception,
+        meta: {
+          menu: true,
+          title: '异常考勤查询',
+          icon: 'warning',
+          auth: true,
+        }
+      },
+      {
+        path: 'apply',
+        name: 'apply',
+        component: Apply,
+        meta: {
+          menu: true,
+          title: '添加考勤审批',
+          icon: 'document-add',
+          auth: true,
+        }
+      },
+      {
+        path: 'check',
+        name: 'check',
+        component: Check,
+        meta: {
+          menu: true,
+          title: '我的考勤审批',
+          icon: 'finished',
+          auth: true,
+        }
+      }
+    ]
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/login',
+    name: 'login',
+    component: Login
   }
 ]
 
