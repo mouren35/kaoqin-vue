40ccf56d16212488ccef38670223b19d7a7c56f6
d <new666@qq.com>
2022-09-28 22:19:09 +0800

    Vue3+TS4项目 - 登录页与首页实现

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
 import type { AxiosRequestConfig, AxiosResponse } from 'axios'
+import store from '@/store';
+import type { StateAll } from '@/store'
+import { ElMessage } from 'element-plus';
 
 const instance = axios.create({
   baseURL: 'http://localhost:3000/',
@@ -7,12 +10,22 @@ const instance = axios.create({
 });
 
 instance.interceptors.request.use(function (config) {
+  if(config.headers){
+    config.headers.authorization = (store.state as StateAll).users.token;
+  }
   return config;
 }, function (error) {
   return Promise.reject(error);
 });
 
 instance.interceptors.response.use(function (response) {
+  if(response.data.errmsg === 'token error'){
+    ElMessage.error('token error');
+    store.commit('users/clearToken');
+    setTimeout(()=>{
+      window.location.replace('/login');
+    }, 1000)
+  }
   return response;
 }, function (error) {
   return Promise.reject(error);
